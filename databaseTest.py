4b95b883ecc5e9c2c39dc3da1f6b3f07fd79c70f
Jax Choong <choongjax@gmail.com>
2024-03-31 14:43:04 +0800

    style: added comments

--- databaseTest.py
+++ databaseTest.py
@@ -4,14 +4,11 @@ db = SQL("sqlite:///database.db")
 
 users = db.execute("SELECT * FROM USERS")
 
+# Hard coded KEYS just in case
 KEYS = ["id","username","password", "position"]
-# print(users[0]["id"])
-# print(users[0]["username"])
-# print(users[0]["password"])
-# print(users[0]["position"])
 values = list(users[0].values())
 
-
+# Prints KEYS into csv first
 file = open("csv.txt","w")
 for i in range(len(KEYS)):
   if i < len(KEYS) -1 :
@@ -19,6 +16,7 @@ for i in range(len(KEYS)):
   else:
     file.write(KEYS[i] +"\n")
 
+# Writes data into csv  (for now just 1 line)
 for i in range(len(values)):
   if i < len(values) -1 :
     file.write(str(values[i]) + ",")
