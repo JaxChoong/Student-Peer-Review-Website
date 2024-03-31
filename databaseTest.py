b65a1a976f2e7067d95a3e554b118b0ecde1698f
Jax Choong <choongjax@gmail.com>
2024-03-31 19:42:48 +0800

    style: refactored code into a function to be used in main code

--- databaseTest.py
+++ databaseTest.py
@@ -12,20 +12,19 @@ users = db.execute("SELECT * FROM USERS")
 # Hard coded KEYS just in case
 KEYS = ["id","username","password", "position"]
 
+# Copy this function into the main code
+def writeToCsv():
+  with open("databaseToCsv.txt", "w", newline='') as file:    # opens txt file and newline is empty to prevent "\n" to be made automatically
+    writer = csv.writer(file)                               # creates writer to write into file as csv 
+    
+    # Write table header  with hardcoded KEYS constant 
+    writer.writerow(KEYS) 
+    
+    # Write data rows
+    for user in users:
+      writer.writerow(user.values())
+  file.close()
 
-
-with open("databaseToCsv.txt", "w", newline='') as file:    # opens txt file and newline is empty to prevent "\n" to be made automatically
-  writer = csv.writer(file)                               # creates writer to write into file as csv 
-  
-  # Write table header  with hardcoded KEYS constant 
-  writer.writerow(KEYS) 
-  
-  # Write data rows
-  for user in users:
-    writer.writerow(user.values())
-
-
-file.close()
-
+writeToCsv()
 file = open("databaseToCsv.txt","r")
 print(file.read())
