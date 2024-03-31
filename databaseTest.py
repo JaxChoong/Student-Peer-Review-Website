345ca89da76a058bdd96e7eb70ef812f1a6e57f4
Jax Choong <choongjax@gmail.com>
2024-03-31 15:10:39 +0800

    feature: added commands to add users to database

--- databaseTest.py
+++ databaseTest.py
@@ -3,6 +3,11 @@ from cs50 import SQL
 db = SQL("sqlite:///database.db")
 
 users = db.execute("SELECT * FROM USERS")
+
+# RUN THIS TO ADD ANOTHER USER TO DATABASE
+# db.execute("INSERT INTO USERS(username,password,position) VALUES(?,?,?)", 'Eric','000','STUDENT')
+
+
 # Hard coded KEYS just in case
 KEYS = ["id","username","password", "position"]
 
@@ -15,13 +20,15 @@ for i in range(len(KEYS)):
     file.write(KEYS[i] +"\n")
 
 # Writes data into Csv  (for now just 1 line)
-for i in range(len(users)):
+for i in range(len(users)):            # Loop through users in the database
   values = list(users[i].values())
-  for j in range(len(values)):
+  for j in range(len(values)):         # Loop through each value of the user
     if j < len(values) -1 :
       file.write(str(values[j]) + ",")
     else:
-      file.write(str(values[j]) +"\n")
+      file.write(str(values[j]))
+      if i < len(users)-1:             # If its not last user, then add newline after last value of current user.
+        file.write("\n")
 
 file.close()
 
