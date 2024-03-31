1a92a98a2a5f43676360c52767dbec487b8ab420
Jax Choong <choongjax@gmail.com>
2024-03-31 15:34:14 +0800

    style: shortened the code but now there is newline after final user

--- databaseTest.py
+++ databaseTest.py
@@ -1,4 +1,5 @@
 from cs50 import SQL
+import csv
 
 db = SQL("sqlite:///database.db")
 
@@ -11,24 +12,18 @@ users = db.execute("SELECT * FROM USERS")
 # Hard coded KEYS just in case
 KEYS = ["id","username","password", "position"]
 
-# Prints KEYS into Csv first
-file = open("databaseToCsv.txt","w")
-for i in range(len(KEYS)):
-  if i < len(KEYS) -1 :
-    file.write(KEYS[i] + ",")
-  else:
-    file.write(KEYS[i] +"\n")
 
-# Writes data into Csv  (for now just 1 line)
-for i in range(len(users)):            # Loop through users in the database
-  values = list(users[i].values())
-  for j in range(len(values)):         # Loop through each value of the user
-    if j < len(values) -1 :
-      file.write(str(values[j]) + ",")
-    else:
-      file.write(str(values[j]))
-      if i < len(users)-1:             # If its not last user, then add newline after last value of current user.
-        file.write("\n")
+
+with open("databaseToCsv.txt", "w", newline='') as file:    # opens txt file and newline is empty to prevent "\n" to be made automatically
+  writer = csv.writer(file)                               # creates writer to write into file as csv 
+  
+  # Write table header  with hardcoded KEYS constant 
+  writer.writerow(KEYS) 
+  
+  # Write data rows
+  for user in users:
+    writer.writerow(user.values())
+
 
 file.close()
 
