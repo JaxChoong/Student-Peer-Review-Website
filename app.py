9b6a11befb42efe3bb982e626c34b30a3342a26a
iconicstixx <ccalvinoutlookcom@gmail.com>
2024-04-20 13:26:05 +0800

    feat: funky log in

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 @app.route("/")
 def index():
+    return render_template("home.html", name=session.get("name"))
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+            session["name"] = request.form.get("UserId")
+            return redirect("/")
     return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
