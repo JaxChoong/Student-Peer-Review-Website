19b41e9987f81d722a5718d84d167c9fb689d2dd
Jax Choong <choongjax@gmail.com>
2024-04-21 11:56:20 +0800

    feature: added google account login

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from flask import Flask, redirect, render_template, request, session
+import os
+import pathlib
+from flask import Flask, redirect, render_template, request, session, abort
 from flask_session import Session
+from google_auth_oauthlib.flow import Flow
+import requests
+from google.oauth2 import id_token
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
 
 app = Flask(__name__)
 
@@ -7,18 +14,45 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+GOOGLE_CLIENT_ID = "713939446938-bnel24iohi6clskjameibirdqdd2533h.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+    )
+
+
+def login_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)          # google authorization required
+        else:
+            return function()
+    return wrapper
+    
 @app.route("/")
 def index():
     return render_template("home.html", name=session.get("name"))
 
 @app.route("/login", methods=["GET","POST"])
 def login():
-    if request.method == "POST":
-            session["name"] = request.form.get("UserId")
-            return redirect("/")
-    return render_template("login.html")
+    session["name"] = request.form.get("UserId")
+    session["google_id"] = "foo"
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
 
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
+
+@app.route("/protected_area")
+@login_required
+def protected_area():
+    return "protected"
+
+@app.route("/callback")
+def callback():
+    return "hey it works!"
