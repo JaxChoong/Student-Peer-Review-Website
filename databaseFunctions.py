c7a8ed5870bbc9fd591385a75d88249605ac84d9
Jax Choong <choongjax@gmail.com>
2024-04-21 15:59:25 +0800

    style: renamed existingUsers to existingEmails

--- databaseFunctions.py
+++ databaseFunctions.py
@@ -4,10 +4,8 @@ import csv
 con = sqlite3.connect("database.db")      # connects to the database
 db = con.cursor()                         # cursor to go through database (allows db.execute() basically)
 
-# users = db.execute("SELECT * FROM users")
-# users = db.fetchall()          # get all the users cuz this library doesnt do it for you
-existingUsers = db.execute("SELECT email FROM users")
-existingUsers = list({user[0] for user in existingUsers})    # turn existing users into a list
+existingEmails = db.execute("SELECT email FROM users")
+existingEmails = list({user[0] for user in existingEmails})    # turn existing users into a list
 
 
 # Hard coded KEYS just in case
@@ -16,6 +14,8 @@ ROLES = ["STUDENT","LECTURER"]
 
 # Copy this function into the main code
 def databaseToCsv():
+  users = db.execute("SELECT * FROM users")
+  users = db.fetchall()          # get all the users cuz this library doesnt do it for you
   with open("databaseToCsv.txt", "w", newline='') as file:    # opens txt file and newline is empty to prevent "\n" to be made automatically
     writer = csv.writer(file)                               # creates writer to write into file as csv 
     
@@ -23,7 +23,6 @@ def databaseToCsv():
     writer.writerow(KEYS) 
     
     # Write data rows
-    print(f"{existingUsers}")
     for user in users:
       writer.writerow(user)
   file.close()
@@ -62,7 +61,7 @@ def csvToDatabase():
       if userRole not in ROLES:      # check if user Role exists
         print(f"Role {userRole} does not exist.")
         continue
-      elif ( email) not in existingUsers and row:  # if user not already existing and not empty row
+      elif ( email) not in existingEmails and row:  # if user not already existing and not empty row
         db.execute("INSERT INTO users (id,name,email,role) VALUES(?,?,?,?)",(user_id, name,email,userRole))
         con.commit()
         print("added to database")
