58a2ed5917a6654f464d43bbac8fa754553986ff
Jax Choong <choongjax@gmail.com>
2024-04-29 12:15:07 +0800

    fix: fixed whatever main was messed up with

--- databaseFunctions.py
+++ databaseFunctions.py
@@ -1,21 +1,21 @@
 import sqlite3
 import csv
 
-con = sqlite3.connect("database.db")      # connects to the database
+con = sqlite3.connect("database.db", check_same_thread=False)      # connects to the database
 db = con.cursor()                         # cursor to go through database (allows db.execute() basically)
 
-users = db.execute("SELECT * FROM USERS")
-users = db.fetchall()          # get all the users cuz this library doesnt do it for you
-existingUsers = db.execute("SELECT id,username FROM USERS")
-existingUsers = list({(user[0],user[1]) for user in existingUsers})    # turn existing users into a list
+existingEmails = db.execute("SELECT email FROM users")
+existingEmails = list({user[0] for user in existingEmails})    # turn existing users into a list
 
 
 # Hard coded KEYS just in case
-KEYS = ["id","username","password", "position"]
-POSITIONS = ["STUDENT","LECTURER"]
+KEYS = ["id","name","email", "role"]
+ROLES = ["STUDENT","LECTURER"]
 
 # Copy this function into the main code
 def databaseToCsv():
+  users = db.execute("SELECT * FROM users")
+  users = db.fetchall()          # get all the users cuz this library doesnt do it for you
   with open("databaseToCsv.txt", "w", newline='') as file:    # opens txt file and newline is empty to prevent "\n" to be made automatically
     writer = csv.writer(file)                               # creates writer to write into file as csv 
     
@@ -23,7 +23,6 @@ def databaseToCsv():
     writer.writerow(KEYS) 
     
     # Write data rows
-    print(f"{existingUsers}")
     for user in users:
       writer.writerow(user)
   file.close()
@@ -44,7 +43,7 @@ def csvToDatabase():
         foundEmptyValue = True
         continue
 
-      for data in row:           
+      for data in row:         
         if not data:         #checks if data coloumn is empty      
           foundEmptyValue = True
           print(f"Empty value found in row {row}. Skipping...")
@@ -53,19 +52,21 @@ def csvToDatabase():
       if foundEmptyValue == True:
         continue                   # skips this cycle of the loop
       
-      # get current userid and username
+      # get current userid and name
       user_id= int(row[0])
-      username= row[1]
-      userPosition = row[3]
-      userPosition= userPosition.upper()
-      if userPosition not in POSITIONS:      # check if user position exists
-        print(f"Position {userPosition} does not exist.")
+      name= row[1]
+      email = row[2]
+      userRole = row[3]
+      userRole= userRole.upper()
+      if userRole not in ROLES:      # check if user Role exists
+        print(f"Role {userRole} does not exist.")
         continue
-      elif ( user_id,username) not in existingUsers and row:  # if user not already existing and not empty row
+      elif ( email) not in existingEmails and row:  # if user not already existing and not empty row
+        db.execute("INSERT INTO users (id,name,email,role) VALUES(?,?,?,?)",(user_id, name,email,userRole))
+        con.commit()
         print("added to database")
-        db.execute("INSERT INTO USERS (id,username,password,position) VALUES(?,?,?,?)",user_id, username,row[2],userPosition)
       else:
-        print(f"User {user_id}, {username} already Exists.")
+        print(f"User {user_id}, {name} already Exists.")
   file.close()
 
 def checkEmail(session):
