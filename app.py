5a2c68ac3fc441b49d82a5d86fb28aad31b7b008
Jax Choong <choongjax@gmail.com>
2024-05-04 13:05:52 +0800

    feat: now the flask app automatically refreshes after u have made changes ( also u can just press f5 to run it now)

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-    
+
 con = sqlite3.connect("database.db", check_same_thread=False)      # connects to the database
 db = con.cursor()
 
@@ -55,3 +55,6 @@ def studentGroups():
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html", name=session.get("username"))
+
+if __name__ == "__main__":
+    app.run(debug=True)    # has auto refresh now 
