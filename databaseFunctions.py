4bcc82f1fa2743545d31c0318546b1c7da0d8f1c
Jax Choong <choongjax@gmail.com>
2024-05-05 11:44:09 +0800

    feat: now checks if there is at least one capital letter

--- databaseFunctions.py
+++ databaseFunctions.py
@@ -135,10 +135,11 @@ def checkPasswords(currentPassword,newPassword,confirmPassword,session):
   if not currentPassword or not newPassword or not confirmPassword:
     flash("INPUT FIELDS ARE EMPTY!")
     return redirect("/changePassword")
-  elif not re.match(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", newPassword):
+  elif not re.match(r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$", newPassword):
     # ^ => start of string
     # checks if password contains at both alphabets and numbers, and also if it is 8 characters long
-    # (?=.*[A-Za-z])=> checks if there is alphabets
+    # (?=.*[A-Z]) => checks if there is at least one capital letter
+    # (?=.*[a-z]) => checks if there is at least one small letter
     # (?=.*\d) => checks if there are digits
     # [A-Za-z\d]{8,} => checks if the newPassword has a combination of alphabets and numbers that is 8 char long
     # $ => end of string
